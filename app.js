9abab8c53f6690013aa21873cc28207ad847d94d
pivnati <natanmuleta911@gmail.com>
2025-03-04 23:41:57 -0700

    modify it

--- app.js
+++
@@ -1,9 +0,0 @@
-const EventEmitter = require('events');
-
-const eventemitter = new EventEmitter();
-
-eventemitter.on('greet', (name)=>{
-   console.log( `Hello ${name}`); });
-
-   eventemitter.emit('greet', 'John');
-    eventemitter.emit('greet', 'Jane');  
