eff1b0bbd960cb1ecb1d6d8d522708169b5d79b3
pivnati <natanmuleta911@gmail.com>
2025-04-04 09:48:11 -0700

    create the router dart file

---
+++ lib/router.dart
@@ -0,0 +1,8 @@
+import 'package:amazon_clone/features/auth/screens/auth_screen.dart';
+import 'package:flutter/material.dart';
+
+Route<dynamic> generateRoute(RouteSettings,routeSettings){
+  switch(routeSettings.name){
+      case AuthScreen.routeName:
+  }
+}
