3e28a4c828a6feed7bcbabf15a0e88497e69c40d
pivnati <natanmuleta911@gmail.com>
2025-04-19 14:22:33 -0700

    create the auth.js in the routes folder

---
+++ server/index.js
@@ -0,0 +1,13 @@
+console.log('hi')
+const express = require('express')
+const PORT = 3000
+const app = express();
+
+
+app.get('/' ,( req,res)=>{
+     res.send('hey wassup')
+})   
+app.listen(PORT, ()=>{
+    console.log('Connected at port '+ PORT);
+});
+
