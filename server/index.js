c9e6da52720e5725a10ee1abe3eaa84d1266ab9b
pivnati <natanmuleta911@gmail.com>
2025-04-18 19:03:30 -0700

    create the connection with the mogo db

--- server/index.js
+++ server/index.js
@@ -1,9 +1,22 @@
 import express from 'express'
 import { authRouter } from './routes/auth.js';
+import mongoose from 'mongoose';
 
  const PORT = 3000;
  const app = express();
+ const DB = "mongodb+srv://natan_Piv123:pivnati123%23@cluster0.fepwjp9.mongodb.net"
+
  
+// connection of the db 
+
+mongoose.connect(DB)
+  .then(() => {
+    console.log('Connection successful');
+  })
+  .catch((e) => {
+    console.log('Failed to connect:', e); // <-- show real error
+  });
+
 app.use(authRouter)
  app.listen(PORT, ()=>{
     console.log(`connected at port ${PORT}`)
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,4 +1,8 @@
 import express from 'express'
 
 export const authRouter = express.Router()
- 
+
+authRouter.post('/api/signup', (req,res)=>{
+    
+    //
+ })
